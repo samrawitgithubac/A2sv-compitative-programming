9299b4dc2a7f8c2418d7ec8f01a6c188e0734acf
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2024-09-26 05:18:17 -0700

    Time: 25 ms (96.43%), Space: 16.4 MB (63.01%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,20 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        # Initialize the first two steps
+        first = 1
+        second = 2
+        
+        # Iterate and calculate the number of ways for each step
+        for i in range(3, n+1):
+            third = first + second  # The current number of ways
+            first = second  # Move the first pointer
+            second = third  # Move the second pointer
+        
+        return second  # The final number of ways to reach step `n`
+
+        
