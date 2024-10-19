df1a1746667506c5858882c055c88cec19493a26
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2024-10-19 03:51:03 -0700

    Time: 3 ms (100%), Space: 16.6 MB (57.77%) - LeetHub

---
+++ 0953-reverse-only-letters/0953-reverse-only-letters.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        s=list(s)
+        start=0
+        end=len(s)-1
+        while start<end:
+            while start<end and not s[end].isalpha():
+                end-=1
+            while start<end and not s[start].isalpha():
+                start+=1
+            if s[start].isalpha() and s[end].isalpha():
+                s[start],s[end]=s[end],s[start]
+                start+=1
+                end-=1
+        return ''.join(s)
+        
