bfb78bd9920c577ebdf1f10341bcf91d97b1e064
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2024-11-22 21:59:19 -0800

    Time: 105 ms (34.27%), Space: 27.5 MB (83.63%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,24 +1,23 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-
-        #length of height
-        n=len(height) 
-
-        # starting and last index of array
-        l,r=0,n-1 
-
-        # initailly result is zero
-        res=0 
-
-        while l<r: 
-            #storing max area of water in res for every iteration
-            res=max(res,(r-l)*min(height[l],height[r])) 
-
-            #updating the smallest vertical line hoping 
-            #there would be a bigger vertical line after updation.
-            if height[l]<height[r]: 
-                l+=1
+        i=0
+        j=len(height)-1
+        maxsum=0
+        while i<j:
+            
+            sums=min(height[i],height[j])*(j-i)
+            maxsum=max(maxsum,sums)
+            if height[j]>height[i]:
+               
+                i+=1
             else:
-                r-=1
-        return res
-        
+                
+                j-=1
+        return maxsum
+      
+
+       
+
+
+
+       
