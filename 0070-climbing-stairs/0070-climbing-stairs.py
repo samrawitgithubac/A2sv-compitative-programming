7d53bfee1b7c18edb88cbbe7354cf1256f8a31e7
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2024-12-01 07:56:57 -0800

    Time: 0 ms (100%), Space: 17.1 MB (5.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -8,13 +8,13 @@ class Solution:
         # Initialize the first two steps
         first = 1
         second = 2
-        
+       
         # Iterate and calculate the number of ways for each step
         for i in range(3, n+1):
             third = first + second  # The current number of ways
             first = second  # Move the first pointer
             second = third  # Move the second pointer
-        
+       
         return second  # The final number of ways to reach step `n`
 
         
