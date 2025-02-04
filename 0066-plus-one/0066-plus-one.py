ee13f30135a5798dff16be4e1974a2565719eaa6
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2025-02-03 17:45:58 -0800

    Time: 0 ms (100%), Space: 17.6 MB (65.08%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,19 +1,20 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        # Start from the last digit and iterate backwards
-        for i in range(len(digits) - 1, -1, -1):
-            # If the current digit is less than 9, just add 1 and return
-            if digits[i] < 9:
-                digits[i] += 1
-                return digits
-            # Set the current digit to 0 and continue to the next iteration
-            digits[i] = 0
+        digits=map(str,digits)
         
-        # If all digits were 9, we'll end up here, so we need to add a new 1 at the beginning
-        return [1] + digits
+        digits="".join(digits)
+        digits=int(digits)+1
+        digits=str(digits)
+        res=[]
+        for i in range(len(digits)):
+            res.append(int(digits[i]))
+        return res
 
-            
-
-
-
+       
+       
         
+       
+
+    
+
+       
