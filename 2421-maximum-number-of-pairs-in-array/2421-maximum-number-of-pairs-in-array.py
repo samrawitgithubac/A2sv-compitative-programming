f613d1688e59aee1201b71104ed1e7a71334a32b
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2025-02-06 21:09:42 +0300

    Time: 0 ms (100%), Space: 17.6 MB (74.15%) - LeetHub

--- 2421-maximum-number-of-pairs-in-array/2421-maximum-number-of-pairs-in-array.py
+++ 2421-maximum-number-of-pairs-in-array/2421-maximum-number-of-pairs-in-array.py
@@ -8,8 +8,8 @@ class Solution:
         i = 1  # Initialize i properly
         while i < len(nums):  # Use while to avoid index skipping
             if nums[i - 1] == nums[i]:
-                nums.pop(i)#del nums[i]  # Remove the current element
-                nums.pop(i-1)#del nums[i - 1]  # Remove the previous element
+                del nums[i]  # Remove the current element
+                del nums[i - 1]  # Remove the previous element
                 count += 1  # Increase pair count
             else:
                 i += 1  # Move to the next index
