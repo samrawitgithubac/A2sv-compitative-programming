3ffcaa92815cc59419be5ec0410ea8b0ae1f6994
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2025-02-12 06:59:46 +0300

    Time: 5 ms (21.43%), Space: 17.6 MB (83.87%) - LeetHub

---
+++ 1009-pancake-sorting/1009-pancake-sorting.py
@@ -0,0 +1,18 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        test = arr
+        sort = sorted(arr)
+        n = len(arr)
+        res = []
+        max_ = n-1 # The index were we want to put the max
+
+        while (test != sort):
+            max_index = max(range(n), key=arr.__getitem__)
+            test[0:max_index+1] = test[0:max_index+1][::-1]
+            res.append(max_index+1)
+            test[0:max_+1] = test[0:max_+1][::-1]
+            res.append(max_+1)
+            max_ -=1
+            n-=1
+        
+        return res
