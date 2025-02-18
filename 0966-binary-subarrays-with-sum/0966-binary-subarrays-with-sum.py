e70decbcc44753f8eff3d702dcc4e29baf148475
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2025-02-18 15:22:16 +0300

    Time: 47 ms (6.92%), Space: 21.6 MB (13.36%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
@@ -1,34 +1,32 @@
+from collections import  defaultdict
 class Solution:
-
-    #This is the Updated version of question count subarray with given sum k from Array Playlist
-    #Optimum
-    # To Find number of Subarray which matches goal = number of subarray which are less than goal - number of subarray which are lss than goal - 1
-
-    # Eg Goal less than 2 are 10  - Goal less than goal -1 (2) are 5
-    # so goal equals to 2 are remaining one  
-
-    def numSubarraysWithSumLessThanGoal(self,nums,goal):
-        if goal<0:
-            return 0
-        left, right = 0,0
-        Sum = 0
-        count = 0 
-        
-
-        while right<len(nums):
-            Sum += nums[right]
-
-            # Trim left till we get sum<=goal
-            while Sum>goal:
-                Sum = Sum - nums[left]
-                left+=1
-            # if r at 3 and l at 0 so we wil count all possible subarray we can have in between such as 0th , (0th,1), (0th,1 ,2), (0th,1,2,3)
-            # = 3 - 0 + 1
-            count+= right - left +1
-            right+=1
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        self.nums=nums
+        self.goal=goal
+        prefix=0
+        self.ans=[]
+        for i in range(len(nums)):
+            prefix+=nums[i]
+            self.ans.append(prefix)
+        hashmap1=defaultdict(int)
+        hashmap1[0]=1
+        count=0
+       
+        for i in range(len(self.ans)):
+            if self.ans[i]-self.goal in hashmap1:
+                count+=hashmap1[self.ans[i]-self.goal]
+            hashmap1[self.ans[i]]+=1
         return count
 
-    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        Ans = self.numSubarraysWithSumLessThanGoal(nums,goal) - self.numSubarraysWithSumLessThanGoal(nums,goal-1)
-        return Ans
+
+
+
+    
+
+
+        
+  
+       
+
+        
     
