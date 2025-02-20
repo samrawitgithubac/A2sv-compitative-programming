d9d1cd7c7b19c45b5101c88193ba8ba04c4438ea
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2025-02-20 08:01:28 +0300

    Time: 73 ms (9.66%), Space: 31.1 MB (95.08%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -4,32 +4,37 @@
 #         self.val = val
 #         self.next = next
 
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if not head or not head.next:
-            return True
         
-        # Step 1: Find the middle of the linked list
-        slow, fast = head, head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+        copyss = []
+        temp = head
+        while temp:
+            copyss.append(temp.val)
+            temp = temp.next
+
+       
+        dummy = ListNode(0, head)
+        current = dummy.next
+        pre = None
+        while current:
+            temp = current.next
+            current.next = pre
+            pre = current
+            current = temp
+
         
-        # Step 2: Reverse the second half of the linked list
-        prev = None
-        while slow:
-            next_node = slow.next
-            slow.next = prev
-            prev = slow
-            slow = next_node
-        
-        # Step 3: Compare the first half and the reversed second half
-        first_half, second_half = head, prev
-        while second_half:  # We only need to compare till the end of the second half
-            if first_half.val != second_half.val:
-                return False
-            first_half = first_half.next
-            second_half = second_half.next
-        
-        # If all values matched, it's a palindrome
-        return True
+        reversed_vals = []
+        temp = pre
+        while temp:
+            reversed_vals.append(temp.val)
+            temp = temp.next
+
+        return copyss == reversed_vals  
