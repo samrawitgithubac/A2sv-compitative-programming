bfb4883110b2d20feff4fc3d234f809ab09b8747
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2025-03-09 12:01:37 +0300

    Time: 1 ms (40.18%), Space: 17.8 MB (93%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.py
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.py
@@ -2,15 +2,15 @@
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
         cnt_dict = {}
-        res = 0
+        ans = 0
         for n in answers:
             if n == 0:
-                res+=1
+                ans+=1
             else:
                 if n not in cnt_dict or n== cnt_dict[n]:
                     cnt_dict[n] = 0
-                    res+=n+1
+                    ans+=n+1
                 else:
                     cnt_dict[n] += 1
                     
-        return res
+        return ans
